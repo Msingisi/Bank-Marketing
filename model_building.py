e3205c1fb9f7d0c4ed3e33c684a92f39b19c415e
152769593+Msingisi@users.noreply.github.com <mzingisiratshana@gmail.com>
2024-09-05 00:52:49 +0200

    uploaded model building script

--- model_building.py
+++ model_building.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 31 19:29:58 2024
+Created on Sat Aug 31 19:33:09 2024
 
 @author: mzing
 """
@@ -10,15 +10,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder
 from sklearn.ensemble import BaggingClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 from imblearn.over_sampling import SMOTE
 import joblib 
 import streamlit as st
 
-
-training = pd.read_csv('https://raw.githubusercontent.com/Msingisi/Bank-Marketing/main/dataset/train.csv')
-testing = pd.read_csv('https://raw.githubusercontent.com/Msingisi/Bank-Marketing/main/dataset/test.csv')
+training = pd.read_csv('dataset/train.csv')
+testing = pd.read_csv('dataset/test.csv')
 full_data = pd.concat([training, testing], axis=0) 
 full_data = full_data.sample(frac=1).reset_index(drop=True)
 
@@ -71,22 +72,19 @@ class SkewnessHandler(BaseEstimator, TransformerMixin):
         return X
     
 class BinningNumToYN(BaseEstimator, TransformerMixin):
-    def __init__(self, feat_with_num_enc=['default', 'housing', 'loan']):
+    def __init__(self,feat_with_num_enc=['default','housing','loan']):
         self.feat_with_num_enc = feat_with_num_enc
-
-    def fit(self, df):
+    def fit(self,df):
         return self
-
-    def transform(self, df):
-        if set(self.feat_with_num_enc).issubset(df.columns):
+    def transform(self,df):
+        if (set(self.feat_with_num_enc).issubset(df.columns)):
             # Change 0 to No and 1 to Yes for all the features in feat_with_num_enc
             for ft in self.feat_with_num_enc:
-                df.loc[:, ft] = df[ft].map({1: 'Yes', 0: 'No'})
+                df[ft] = df[ft].map({1:'Yes',0:'No'})
             return df
         else:
             print("One or more features are not in the dataframe")
             return df
-
         
 class MissingValueImputer(BaseEstimator, TransformerMixin):
     def __init__(self, mode_imputed_ft=None, median_imputed_ft=None):
@@ -193,168 +191,29 @@ pipeline = Pipeline([
     ('oversample', Oversample())
 ])
 
-############################# Streamlit interface ############################
-
-st.write("""
-# Bank Marketing Prediction App
-This app predicts if the client will subscribe to a term deposit. Just fill in the customer details and click on the Predict button.
-""")
+# Apply the pipeline to the DataFrame
+train_pipe_prep = pipeline.fit_transform(train_copy)
+test_pipe_prep = pipeline.fit_transform(test_copy)
 
 
-# Age input slider
-st.write("""
-## Age
-""")
-input_age = st.slider('Select customer age', value=30,
-                      min_value=18, max_value=100, step=1)
+# split the train data into X and y (target)
+X_train, y_train = train_pipe_prep.loc[:, train_pipe_prep.columns != 'y'], train_pipe_prep['y'].astype('int64')
+
+# split the test data into X and y (target)
+X_test, y_test = test_pipe_prep.loc[:, test_pipe_prep.columns != 'y'], test_pipe_prep['y'].astype('int64')
 
 
-# Job dropdown
-st.write("""
-## Job
-""")
-jobs = ['Blue-Collar', 'Management', 'Technician', 'Admin', 'Services', 'Retired', 'Self-Employed', 'Entrepreneur', 'Unemployed', 'Housemaid', 'Student']
-input_job = st.selectbox(
-    'Select customer job', jobs)
+# initialize Random forest regression  with default parameters
+bagging_model=BaggingClassifier(n_estimators=15,max_samples=1.0, max_features=0.6, 
+                                bootstrap_features=True, bootstrap=True)
+
+# fit model with training data
+bagging_model.fit(X_train, y_train)
+
+# make predictions
+prediction=bagging_model.predict(X_test)
+
+print('Model Accuracy:', accuracy_score(prediction, y_test))
 
 
-# Mariral status input
-st.write("""
-## Marital status
-""")
-input_marital_status = st.radio('Select customer marital status',['Married','Single', 'Divorced'], index=0)
-
-
-# Education input
-st.write("""
-## Education
-""")
-input_education = st.radio('Select customer education',['Secondary','Tertiary', 'Primary'], index=0)
-
-
-# default input
-st.write("""
-## Default
-""")
-input_default = st.radio('Has Credit in Default?',['Yes','No'], index=0)
-
-
-# Balance
-st.write("""
-## Balance
-""")
-customer_balance = st.number_input('Average Yearly Balance', value=1000)
-
-
-# Housing input
-st.write("""
-## Housing
-""")
-input_housing = st.radio('Has Housing Loan?',['Yes','No'], index=0)
-
-
-# Loan input
-st.write("""
-## Loan
-""")
-input_loan = st.radio('Has Personal Loan?',['Yes','No'], index=0)
-
-
-# contact input
-st.write("""
-## Contact Method
-""")
-input_contact = st.radio('Contact Communication Type',['Cellular','Telephone'], index=0)
-
-
-
-# Duration
-st.write("""
-## Duration 
-""")
-customer_duration = st.number_input('Last Contact Duration (seconds)', value=100)
-
-
-
-# outcome input
-st.write("""
-## Previous Outcome
-""")
-input_outcome = st.radio('Outcome of Previous Campaign',['Failure','Other', 'Success'], index=0)
-
-
-st.markdown('##')
-st.markdown('##')
-
-
-# list of all the inputs
-profile_to_predict = [input_age, input_job, input_marital_status, input_education, input_default, customer_balance,
-                      input_housing, input_loan, input_contact, 0, '', customer_duration,
-                      0, 0, 0, input_outcome, 0, '']
-
-
-# Convert to dataframe with column names
-profile_to_predict_df = pd.DataFrame(
-    [profile_to_predict], columns=train_copy.columns)
-
-
-train_copy_with_profile_to_pred = pd.concat([train_copy,profile_to_predict_df],ignore_index=True)
-
-
-train_copy_prep = pipeline.fit_transform(train_copy)
-
-test_copy_prep = pipeline.fit_transform(test_copy)
-
-X_train_copy_prep = train_copy_prep.iloc[:,:-1]
-
-y_train_copy_prep = train_copy_prep.iloc[:,-1]
-
-
-X_test_copy_prep = test_copy_prep.iloc[:,:-1]
-
-
-y_test_copy_prep = test_copy_prep.iloc[:,-1]
-
-
-
-train_copy_with_profile_to_pred = pipeline.fit_transform(train_copy_with_profile_to_pred)
-
-profile_to_pred_prep = train_copy_with_profile_to_pred.iloc[-1:,:-1]
-
-
-# Function to load the model
-def load_model():
-    try:
-        model = joblib.load('bagging.sav')
-        return model
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
-        return None
-    
-# Predict button
-if st.button('Predict'):
-    with st.spinner('Predicting...'):
-        loaded_model = load_model()
-        if loaded_model:
-            prediction = loaded_model.predict(profile_to_pred_prep)
-            prediction_proba = loaded_model.predict_proba(profile_to_pred_prep)
-            formatted_prediction = 'The client will subscribe to the term deposit.' if prediction[0] == 1 else 'The client will not subscribe to the term deposit.'
-            probability = prediction_proba[0][1] if prediction[0] == 1 else prediction_proba[0][0]
-            st.markdown(f"**Prediction: {formatted_prediction}**", unsafe_allow_html=True)
-            st.markdown(f"**Probability of this prediction: {probability:.2%}**", unsafe_allow_html=True)
-            st.markdown(f"**Client Profile Summary:**")
-            st.markdown(f"- **Age**: {input_age}")
-            st.markdown(f"- **Job**: {input_job}")
-            st.markdown(f"- **Marital Status**: {input_marital_status}")
-            st.markdown(f"- **Education**: {input_education}")
-            st.markdown(f"- **Credit Default**: {input_default}")
-            st.markdown(f"- **Average Yearly Balance**: €{customer_balance:,.2f}")
-            st.markdown(f"- **Housing Loan**: {input_housing}")
-            st.markdown(f"- **Personal Loan**: {input_loan}")
-            st.markdown(f"- **Contact Communication Type**: {input_contact}")
-            st.markdown(f"- **Duration of Last Contact**: {customer_duration} seconds")
-            st.markdown(f"- **Outcome of Previous Campaign**: {input_outcome}")
-            
-            # Display balloons if the client will subscribe
-            if prediction[0] == 1:
-                st.balloons()
+joblib.dump(bagging_model, "bagging.sav")
